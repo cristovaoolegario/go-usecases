b8dc9f25dc60421accc4da40290748b649421656
Cristóvão Olegário <cristovaoolegario@gmail.com>
2022-11-10 23:45:33 -0300

    chore: add Order repository

---
+++ internal/infra/database/order_repository.go
@@ -0,0 +1,29 @@
+package database
+
+import (
+	"database/sql"
+
+	entity "github.com/cristovaoolegario/go-usecases/internal/order"
+)
+
+type OrderRepository struct {
+	Db *sql.DB
+}
+
+func NewOrderRepository(db *sql.DB) *OrderRepository {
+	return &OrderRepository{Db: db}
+}
+
+func (r *OrderRepository) Save(order *entity.Order) error {
+	stmt, err := r.Db.Prepare("INSERT INTO orders (id, price, tax, final_price) VALUES (?, ?, ?, ?)")
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec(order.ID, order.Price, order.Tax, order.FinalPrice)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
